87aa3cf25f2f5d6eea859a1eeb68500ebe1d1c98
Leonor <leonorpatriciasf@hotmail.com>
2020-04-29 19:29:56 +0100

    Relógio

---
+++ Chronos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch cron1, cron2;
+
+            cron1 = new Stopwatch();
+            cron2 = new Stopwatch();
+
+            cron1.Start();
+            Thread.Sleep(1000);
+
+            cron2.Start();
+            Thread.Sleep(1000);
+
+            cron1.Stop();
+            cron2.Stop();
+
+            Console.WriteLine($"Tempo cron1 = {cron1.ElapsedMilliseconds}");
+            Console.WriteLine($"Tempo cron1 = {cron2.ElapsedMilliseconds}");
+        }
+    }
+}
